54b3cb957f3fbe722ef187ff22fb16bf61a88f48
Reza IF <rezaif79.ri@gmail.com>
2024-01-31 18:06:26 +0700

    feat: add message controller to main router

--- app/producer/router/router.go
+++ app/producer/router/router.go
@@ -2,12 +2,14 @@ package router
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"gitlab.com/rezaif79-ri/go-rabbitmq-101/app/producer/controller"
 	"gitlab.com/rezaif79-ri/go-rabbitmq-101/internal/rabbitdev"
 )
 
 func SetupMainRouter(router fiber.Router, rbmdConn *rabbitdev.RabbitMqDev) {
 	messageRoute := router.Group("messages")
-	messageRoute.Post("messages", func(c *fiber.Ctx) error {
-		return nil
-	})
+
+	messageController := controller.InitMessageController(rbmdConn)
+
+	messageRoute.Post("messages", messageController.SendMessageToServer)
 }
