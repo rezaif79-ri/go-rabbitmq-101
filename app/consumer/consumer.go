83cbcecfbf7c3d7afda320487bba88e0fe7b4d87
Reza IF <rezaif79.ri@gmail.com>
2024-02-08 18:17:37 +0700

    feat: add v2 consumer send message

--- app/consumer/consumer.go
+++ app/consumer/consumer.go
@@ -33,6 +33,21 @@ func main() {
 
 	fmt.Println("check queue: ", queue)
 
+	// declaring queue with its properties over the the channel opened
+	queue, err = channel.QueueDeclare(
+		"sendMessageV2", // name
+		false,           // durable
+		false,           // auto delete
+		false,           // exclusive
+		false,           // no wait
+		nil,             // args
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("check queue v2: ", queue)
+
 	// declaring consumer with its properties over channel opened
 	msgs, err := channel.Consume(
 		"sendMessage", // queue
@@ -47,6 +62,20 @@ func main() {
 		panic(err)
 	}
 
+	// declaring consumer with its properties over channel opened
+	msgsV2, err := channel.Consume(
+		"sendMessageV2", // queue
+		"",              // consumer
+		true,            // auto ack
+		false,           // exclusive
+		false,           // no local
+		false,           // no wait
+		nil,             //args
+	)
+	if err != nil {
+		panic(err)
+	}
+
 	// print consumed messages from queue
 	forever := make(chan bool)
 	go func() {
@@ -55,6 +84,13 @@ func main() {
 			fmt.Println("deliv tag:", msg.DeliveryTag)
 			fmt.Println("message id:", msg.MessageId)
 			fmt.Printf("Received Message: %s\n", msg.Body)
+		}
+
+		for msg := range msgsV2 {
+			fmt.Println("V2 - cons tag:", msg.ConsumerTag)
+			fmt.Println("V2 - deliv tag:", msg.DeliveryTag)
+			fmt.Println("V2 - message id:", msg.MessageId)
+			fmt.Printf("V2 - Received Message: %s\n", msg.Body)
 		}
 	}()
 
